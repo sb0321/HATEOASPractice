5bf3b9cdd39aaf2999c9a758d15b0e4eca17ddd4
sb0321 <woltaryksb1@gmail.com>
2021-04-09 16:58:30 +0900

    init commit

---
+++ src/test/java/me/whiteship/commomweb/CommomwebApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.commomweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommomwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
